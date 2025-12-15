937e3b7dc2bf47c9303b698561f454d47701d58f
snehadeep-wagh <snehdeepwagh1234@gmail.com>
2025-12-15 17:04:27 +0530

    Removed the map from util nd added to the notificationFactory.

--- Practice problems/Notification Service - self/NotificationFactory/NotificationChannelFactory.java
+++ Practice problems/Notification Service - self/NotificationFactory/NotificationChannelFactory.java
@@ -4,15 +4,28 @@ import NotificationService.EmailNotification;
 import NotificationService.NotificationService;
 import NotificationService.PushNotification;
 import NotificationService.SmsNotification;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import Exceptions.UnsupportedNotificationType;
 import enums.NotificationChannelEnum;
 import utils.notificationUtil;
 
 public class NotificationChannelFactory {
+    private static Map<NotificationChannelEnum, NotificationService> notificationChannelToServiceMap;
+
+    static {
+        notificationChannelToServiceMap = new HashMap<>();
+        notificationChannelToServiceMap.put(NotificationChannelEnum.SMS, new SmsNotification());
+        notificationChannelToServiceMap.put(NotificationChannelEnum.EMAIL, new EmailNotification());
+        notificationChannelToServiceMap.put(NotificationChannelEnum.PUSH, new PushNotification());
+    }
 
     public NotificationService getNotificationChannel(NotificationChannelEnum notificationType) {
-        if (notificationUtil.notificationChannelToServiceMap.get(notificationType) != null) {
-            return notificationUtil.notificationChannelToServiceMap.get(notificationType);
+        NotificationService notificationService = notificationChannelToServiceMap.get(notificationType);
+        if (notificationService != null) {
+            return notificationService;
         } else {
             throw new UnsupportedNotificationType();
         }
--- Practice problems/Notification Service - self/utils/notificationUtil.java
+++ Practice problems/Notification Service - self/utils/notificationUtil.java
@@ -11,7 +11,6 @@ import enums.NotificationChannelEnum;
 
 public class notificationUtil {
     public static Map<Integer, NotificationChannelEnum> notificationChannelMap;
-    public static Map<NotificationChannelEnum, NotificationService> notificationChannelToServiceMap;
 
     static {
         notificationChannelMap = new HashMap<>();
@@ -19,9 +18,5 @@ public class notificationUtil {
         notificationChannelMap.put(2, NotificationChannelEnum.EMAIL);
         notificationChannelMap.put(3, NotificationChannelEnum.PUSH);
 
-        notificationChannelToServiceMap = new HashMap<>();
-        notificationChannelToServiceMap.put(NotificationChannelEnum.SMS, new SmsNotification());
-        notificationChannelToServiceMap.put(NotificationChannelEnum.EMAIL, new EmailNotification());
-        notificationChannelToServiceMap.put(NotificationChannelEnum.PUSH, new PushNotification());
     }
 }
